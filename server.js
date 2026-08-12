7df9c840d332091896f0919cc96012a7a569547e
Kushal909 <kushalukumar909@gmail.com>
2026-08-12 15:40:52 +0530

    updated server and jenkins file

--- server.js
+++ server.js
@@ -1,75 +1,41 @@
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8"]);
+
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
 app.use(express.json());
 
-// Home
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error("MONGO_URI is missing in .env");
+}
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  });
+
 app.get("/", (req, res) => {
   res.json({
-    message: "AWS Node.js Demo API is running 🚀",
-    version: "1.0.0"
+    success: true,
+    message: "Node.js + MongoDB API is working",
   });
-});
-
-// Health check
-app.get("/health", (req, res) => {
-  res.json({
-    status: "OK",
-    message: "Server is healthy"
-  });
-});
-
-// Get tasks
-app.get("/tasks", (req, res) => {
-  const tasks = [
-    {
-      id: 1,
-      title: "Learn AWS",
-      completed: false
-    },
-    {
-      id: 2,
-      title: "Learn Docker",
-      completed: false
-    },
-    {
-      id: 3,
-      title: "Learn CI/CD",
-      completed: false
-    }
-  ];
-
-  res.json(tasks);
-});
-
-// Create task
-app.post("/tasks", (req, res) => {
-  console.log("body", req.body);
-
-  const { title } = req.body;
-
-  console.log("tasks", title);
-
-  if (!title) {
-    return res.status(400).json({
-      message: "Title is required"
-    });
-  }
-
-  res.status(201).json({
-    message: "Task created successfully",
-    task: {
-      id: Date.now(),
-      title,
-      completed: false
-    }
-  });
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
